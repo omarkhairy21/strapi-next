d7c1fb6d602fbcac9cfa08fb42f1e57e3ccb6955
Omar Khairy <omarkhairy21@gmail.com>
2021-12-28 19:40:49 +0200

    Add All

---
+++ back/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e420f0a13558df60116ec2cbf4fd275e'),
+    },
+  },
+});
